c36a36c07f0ef9720ee04e23f395b97b121180ba
coschmitt <charlieschmitt123@gmail.com>
2020-09-17 16:52:49 -0500

    changed sign up html

--- FairBnb/appone/urls.py
+++ FairBnb/appone/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name='home'),
     path("sign-up", views.SignUp.as_view(), name="sign-up"),
+    path("sign-in", views.SignIn.as_view(), name="sign-in"),
+    path("auth_home", views.home, name="auth_home"),
+    path("signout", views.SignOut.as_view(), name='sign-out')
+
 ]
--- FairBnb/appone/views.py
+++ FairBnb/appone/views.py
@@ -1,22 +1,57 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+
 from django.views.generic.edit import FormView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
-    context = {
-        'test':1,
-        'user':'finn'
-    }
-    return render(request, 'appone/index.html', context=context)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/appone/auth_home")
+    else:       # Unregistered User
+        return render(request, 'appone/index.html')
 
 
 class SignUp(FormView):
-    success_url = "/appone/"
+    success_url = "/appone/auth_home"
     form_class = UserCreationForm
     template_name = "appone/signup.html"
 
     def form_valid(self, form):
-        print(form)
         form.save()
-        return super().form_valid(form)
+        user = User.objects.get(username=form.data.get('username'))
+        login(self.request, user)
+        form.save()
+
+        return HttpResponseRedirect("/appone/auth_home")
+
+
+class SignOut(LogoutView):
+    template_name = 'registration/logged_out.html'
+
+
+class SignIn(LoginView):
+    success_url = "/appone/auth_home"
+    form_class = AuthenticationForm
+    template_name = "appone/signin.html"
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        login(self.request, form.get_user())
+        return HttpResponseRedirect("/appone/auth_home")
+
+
+def home(request):
+    user = request.user
+    print(type(user))
+    if not user.is_authenticated:
+        return HttpResponseRedirect("/appone/sign-in")
+    return render(request, 'appone/home.html', context={
+        'user':user
+    })
+
